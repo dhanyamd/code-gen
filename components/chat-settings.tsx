d6ef087ad2f7bdfd0654b9fe47ee876e35163967
unknown <dhanya13md@gmail.com>
2024-12-11 19:42:16 +0530

    include schema

--- components/chat-settings.tsx
+++ components/chat-settings.tsx
@@ -1,10 +1,11 @@
 import { LLMConfig } from '@/app/models'
 import React from 'react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip'
 import { Button } from './ui/button'
 import { Settings2 } from 'lucide-react'
 import { Label } from './ui/label'
+import { Input } from './ui/input'
  
 const ChatSettings = ({
     apiConfigurable,
@@ -33,12 +34,143 @@ const ChatSettings = ({
     <DropdownMenuContent align='start'>
      {apiConfigurable && (
         <>
-        <div>
+        <div className='flex flex-col gap-2 px-2 py-2'>
             <Label htmlFor='apiKey'> API Key</Label>
-            
+            <Input name='apiKey' type='password' placeholder='Auto' required={true}
+            defaultValue={languageModel.apiKey} 
+            onChange={(e) => 
+              onLanguageModeChange({
+                apiKey : e.target.value.length > 0 ? e.target.value : undefined
+              })
+            }
+            className='text-sm'
+            />
             </div>
+            <DropdownMenuSeparator/>
             </>
      )}
+         {baseURLConfigurable && (
+        <>
+        <div className='flex flex-col gap-2 px-2 py-2'>
+            <Label htmlFor='baseURL'> Base URL</Label>
+            <Input name='baseURL' type='text' placeholder='Auto' required={true}
+            defaultValue={languageModel.baseURL} 
+            onChange={(e) => 
+              onLanguageModeChange({
+                baseURL : e.target.value.length > 0 ? e.target.value : undefined
+              })
+            }
+            className='text-sm'
+            />
+            </div>
+            <DropdownMenuSeparator/>
+            </>
+     )}
+     <div className='flex flex-col gap-1.5 px-2 py-2'>
+        <span className='text-sm font-medium'>Parameters</span>
+        <div className='flex space-x-4 items-center'>
+            <span className='text-sm flex-1 text-muted-foreground'>Output Tokens</span>
+            <Input
+            type='number'
+            defaultValue={languageModel.maxTokens}
+            min={50}
+            max={1000}
+            step={1}
+            placeholder='Auto'
+            className='h-6 rounded-sm w-[84px] text-xs text-center tabular-nums'
+            onChange={(e) => 
+                onLanguageModeChange({
+                    maxTokens : parseFloat(e.target.value) || undefined
+                })
+            }
+            />
+        </div>
+        <div className='flex space-x-4 items-center'>
+            <span className='text-sm flex-1 text-muted-foreground'>Temperature</span>
+            <Input
+            type='number'
+            defaultValue={languageModel.temperature}
+            min={50}
+            max={1000}
+            step={0.01}
+            placeholder='Auto'
+            className='h-6 rounded-sm w-[84px] text-xs text-center tabular-nums'
+            onChange={(e) => 
+                onLanguageModeChange({
+                    temperature : parseFloat(e.target.value) || undefined
+                })
+            }
+            />
+        </div>
+        <div className='flex space-x-4 items-center'>
+            <span className='text-sm flex-1 text-muted-foreground'>Top P</span>
+            <Input
+            type='number'
+            defaultValue={languageModel.topP}
+            min={0}
+            max={1}
+            step={1}
+            placeholder='Auto'
+            className='h-6 rounded-sm w-[84px] text-xs text-center tabular-nums'
+            onChange={(e) => 
+                onLanguageModeChange({
+                    topP : parseFloat(e.target.value) || undefined
+                })
+            }
+            />
+        </div>
+        <div className='flex space-x-4 items-center'>
+            <span className='text-sm flex-1 text-muted-foreground'>Top K</span>
+            <Input
+            type='number'
+            defaultValue={languageModel.topK}
+            min={0}
+            max={500}
+            step={1}
+            placeholder='Auto'
+            className='h-6 rounded-sm w-[84px] text-xs text-center tabular-nums'
+            onChange={(e) => 
+                onLanguageModeChange({
+                    topK : parseFloat(e.target.value) || undefined
+                })
+            }
+            />
+        </div>
+        <div className='flex space-x-4 items-center'>
+            <span className='text-sm flex-1 text-muted-foreground'>Frequence Penalty</span>
+            <Input
+            type='number'
+            defaultValue={languageModel.frequencyPenalty}
+            min={0}
+            max={2}
+            step={0.01}
+            placeholder='Auto'
+            className='h-6 rounded-sm w-[84px] text-xs text-center tabular-nums'
+            onChange={(e) => 
+                onLanguageModeChange({
+                    frequencyPenalty : parseFloat(e.target.value) || undefined
+                })
+            }
+            />
+        </div>
+        <div className='flex space-x-4 items-center'>
+            <span className='text-sm flex-1 text-muted-foreground'>Presence Penalty</span>
+            <Input
+            type='number'
+            defaultValue={languageModel.presencePenalty}
+            min={0}
+            max={2}
+            step={0.01}
+            placeholder='Auto'
+            className='h-6 rounded-sm w-[84px] text-xs text-center tabular-nums'
+            onChange={(e) => 
+                onLanguageModeChange({
+                    presencePenalty : parseFloat(e.target.value) || undefined
+                })
+            }
+            />
+        </div>
+     </div>
     </DropdownMenuContent>
   </DropdownMenu>
 }
